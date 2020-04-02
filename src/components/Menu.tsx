5516e6b6d45f40cb556530189d32d6e59d5123e6
Kapil Raghuwanshi <kraghuwanshi@conversantmedia.com>
2020-04-02 15:21:43 +0530

    Initial dev commit - added platforms and scaffolding

---
+++ src/components/Menu.tsx
@@ -0,0 +1,103 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+} from '@ionic/react';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import './Menu.css';
+
+interface MenuProps extends RouteComponentProps {
+  selectedPage: string;
+}
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+const appPages: AppPage[] = [
+  {
+    title: 'Inbox',
+    url: '/page/Inbox',
+    iosIcon: mailOutline,
+    mdIcon: mailSharp
+  },
+  {
+    title: 'Outbox',
+    url: '/page/Outbox',
+    iosIcon: paperPlaneOutline,
+    mdIcon: paperPlaneSharp
+  },
+  {
+    title: 'Favorites',
+    url: '/page/Favorites',
+    iosIcon: heartOutline,
+    mdIcon: heartSharp
+  },
+  {
+    title: 'Archived',
+    url: '/page/Archived',
+    iosIcon: archiveOutline,
+    mdIcon: archiveSharp
+  },
+  {
+    title: 'Trash',
+    url: '/page/Trash',
+    iosIcon: trashOutline,
+    mdIcon: trashSharp
+  },
+  {
+    title: 'Spam',
+    url: '/page/Spam',
+    iosIcon: warningOutline,
+    mdIcon: warningSharp
+  }
+];
+
+const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+const Menu: React.FunctionComponent<MenuProps> = ({ selectedPage }) => {
+
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+        <IonList id="inbox-list">
+          <IonListHeader>Inbox</IonListHeader>
+          <IonNote>hi@ionicframework.com</IonNote>
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem className={selectedPage === appPage.title ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                  <IonIcon slot="start" icon={appPage.iosIcon} />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonList>
+
+        <IonList id="labels-list">
+          <IonListHeader>Labels</IonListHeader>
+          {labels.map((label, index) => (
+            <IonItem lines="none" key={index}>
+              <IonIcon slot="start" icon={bookmarkOutline} />
+              <IonLabel>{label}</IonLabel>
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default withRouter(Menu);
